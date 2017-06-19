b4122c38f9db723afcec348503151725e3f3f5a0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-19 03:32:18 -0300

    Task 15, Linear search

---
+++ 0x01-big-0-binary-search/0-linear.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
