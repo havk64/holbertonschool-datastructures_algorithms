ba7bc13eb4d622fe1d18b0a2689220a4b893d249
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-02 20:47:43 -0300

    Add docstring to function

--- 0x02-more_search_algorithms/100-jump.c
+++ 0x02-more_search_algorithms/100-jump.c
@@ -17,6 +17,16 @@ void print(int a, int b)
 	printf("Value checked array[%d] = [%d]\n", a, b);
 }
 
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * using the Jump search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: On success, the index where the value was found
+ * On error: return -1
+ */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t a, b;
