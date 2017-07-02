2d5ac12a372ae3e02cf0527a12186e4a26023e3b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-02 02:19:42 -0300

    Jump search, first commit

---
+++ 0x02-more_search_algorithms/100-jump.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+size_t min(size_t a, size_t b)
+{
+	return ((a > b) ? b : a);
+}
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t a, b;
+
+	a = 0;
+	b = (size_t)sqrt(size);
+	while (array[min(b, size) - 1] < value)
+	{
+		a = b;
+		b += sqrt(size);
+		if (a >= size)
+			return (-1);
+	}
+
+	while (array[a] < value)
+	{
+		a++;
+		if (a == min(b, size))
+			return (-1);
+	}
+
+	if (array[a] == value)
+		return (a);
+	else
+		return (-1);
+}
