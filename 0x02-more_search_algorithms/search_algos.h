d2d7654d114ef4c4f2b952e8cb3ce17bf64d1029
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-03 04:23:05 +0000

    Add binary search function

--- 0x02-more_search_algorithms/search_algos.h
+++ 0x02-more_search_algorithms/search_algos.h
@@ -8,4 +8,5 @@ size_t min(size_t a, size_t b);
 void print(int a, int b);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int binary_search(int *array, int start, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
