cd83a2dcf07b43c251b23838e2cd7f46a96cfd5d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-02 22:31:57 -0300

    Add docstring to interpolation search

--- 0x02-more_search_algorithms/102-interpolation.c
+++ 0x02-more_search_algorithms/102-interpolation.c
@@ -1,5 +1,14 @@
 #include "search_algos.h"
 
+/**
+ * interpolation_search - Implements interpolation search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: On success, the first index where value is located
+ * On error, return -1
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos, low, high;
