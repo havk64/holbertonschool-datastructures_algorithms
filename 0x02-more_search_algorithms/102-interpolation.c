b9bee77f770d163913ab49eb5d953897faf6c413
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-02 22:56:42 -0300

    Add print statement

--- 0x02-more_search_algorithms/102-interpolation.c
+++ 0x02-more_search_algorithms/102-interpolation.c
@@ -22,6 +22,7 @@ int interpolation_search(int *array, size_t size, int value)
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
 			     (value - array[low]));
 
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] < value)
 			low = pos + 1;
 		else if (value < array[pos])
