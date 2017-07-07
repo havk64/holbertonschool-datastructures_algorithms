b850024398152ad617353b35f060f2534cb9e66b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-07 03:08:00 -0300

    Add docstring to binary_tree_is_leaf function

--- 0x03-binary_trees/4-binary_tree_is_leaf.c
+++ 0x03-binary_trees/4-binary_tree_is_leaf.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - Check if a node is a leaf
+ * @node: is the pointer to the node to check
+ *
+ * Return: On true, 1. On false, 0
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
