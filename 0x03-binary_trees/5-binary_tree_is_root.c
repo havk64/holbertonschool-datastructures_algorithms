03acf308f4bbd6e27f870e72952d7f9f319ec395
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-07 03:33:03 -0300

    Add docstring to binary_tree_is_root function

--- 0x03-binary_trees/5-binary_tree_is_root.c
+++ 0x03-binary_trees/5-binary_tree_is_root.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_root - Check if a given node is root
+ * @node: pointer to node to be checked
+ *
+ * Return: 1 if the node is root, 0 otherwise
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	return ((node != NULL && node->parent == NULL) ? 1 : 0);
