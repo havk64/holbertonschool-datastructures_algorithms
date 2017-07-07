0b280d1d633971e226ae0a46007888f4bf78bd09
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-06 21:13:21 -0300

    Task 0, first commmit

---
+++ 0x03-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Description
+ * @parent: A pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ *
+ Return: On success, a new node. On error, NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t) * 1);
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
