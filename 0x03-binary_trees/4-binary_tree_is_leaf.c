5f7f30af283ee1450427e9710b04d22283c59e3e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-07 03:11:07 -0300

    Optimize node check

--- 0x03-binary_trees/4-binary_tree_is_leaf.c
+++ 0x03-binary_trees/4-binary_tree_is_leaf.c
@@ -8,10 +8,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-
-	if (node->left == NULL && node->right == NULL)
+	if (node != NULL && node->left == NULL && node->right == NULL)
 		return (1);
 	else
 		return (0);
