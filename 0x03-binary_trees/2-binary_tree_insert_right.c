979b533f6adf1206b6490cdac9e5c1c876023ee0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-07 02:43:24 -0300

    Add docstring to binary_tree_insert_right function

--- 0x03-binary_trees/2-binary_tree_insert_right.c
+++ 0x03-binary_trees/2-binary_tree_insert_right.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - Insert a node as the right child of another node
+ * @parent: a pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Return: On success, the new node. On error, NULL
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
