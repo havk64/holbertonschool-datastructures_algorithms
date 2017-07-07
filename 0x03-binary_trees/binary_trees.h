80b1c1207aef746213783c7c7d2d8a9727f3084e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-06 21:10:20 -0300

    Add binary_trees header file, first commit

---
+++ 0x03-binary_trees/binary_trees.h
@@ -0,0 +1,31 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+/* Basic binary tree */
+typedef struct binary_tree_s binary_tree_t;
+/* Binary search tree */
+typedef struct binary_tree_s bst_t;
+/* AVL tree */
+typedef struct binary_tree_s avl_t;
+/* MAX binary heap */
+typedef struct binary_tree_s heap_t;
+/* Print function */
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif /* BINARY_TREES_H */
