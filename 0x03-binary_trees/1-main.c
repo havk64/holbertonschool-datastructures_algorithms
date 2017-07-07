834bad369b916509d7558368973f6cd08982f0f1
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-07 02:38:05 -0300

    Substitute free_tree with binary_tree_delete

--- 0x03-binary_trees/1-main.c
+++ 0x03-binary_trees/1-main.c
@@ -19,6 +19,6 @@ int main(void)
 	binary_tree_insert_left(root->right, 128);
 	binary_tree_insert_left(root, 54);
 	binary_tree_print(root);
-	free_tree(root);
+	binary_tree_delete(root);
 	return (0);
 }
