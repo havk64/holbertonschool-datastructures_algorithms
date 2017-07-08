8edceef9be5cbbe73758838ea151ad82b38cbba6
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-08 18:07:53 -0300

    Task 8, first commit

---
+++ 0x03-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	if (tree->left != NULL)
+	{
+		binary_tree_postorder(tree->left, func);
+	}
+	if (tree->right != NULL)
+	{
+		binary_tree_postorder(tree->right, func);
+	}
+	(*func)(tree->n);
+}
