4aff8770156f1a177339e119fc7301ca63758f28
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-08 02:16:18 -0300

    Add missing parameter description

--- 0x03-binary_trees/6-binary_tree_preorder.c
+++ 0x03-binary_trees/6-binary_tree_preorder.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_preorder -  goes through a binary tree using pre-order traversal
  * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node
  *
  * Return: always Void
  */
