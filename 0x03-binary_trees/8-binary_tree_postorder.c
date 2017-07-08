bdb8360f4a50ff6a1a8dbfa5c9d194788819109d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-08 18:15:15 -0300

    Add docstring to binary_tree_postorder function

--- 0x03-binary_trees/8-binary_tree_postorder.c
+++ 0x03-binary_trees/8-binary_tree_postorder.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - Goes through a tree using post-order traversal
+ * @tree:  pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ *
+ * Return: always void
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
