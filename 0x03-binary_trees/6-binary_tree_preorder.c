c05828deaf9cbcab759548e1b33cb6c0f3cdf90b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-08 01:35:00 -0300

    Add docstring to binary_tree_preorder

--- 0x03-binary_trees/6-binary_tree_preorder.c
+++ 0x03-binary_trees/6-binary_tree_preorder.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder -  goes through a binary tree using pre-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ *
+ * Return: always Void
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL)
