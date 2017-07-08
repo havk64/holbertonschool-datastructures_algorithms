11ac33054faa805d6f674d6d07ab2ef9271884d0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-08 17:40:31 -0300

    Add docstring to binary_tree_inorder function

--- 0x03-binary_trees/7-binary_tree_inorder.c
+++ 0x03-binary_trees/7-binary_tree_inorder.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to the function to call for each node
+ *
+ * Return: always Void
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
