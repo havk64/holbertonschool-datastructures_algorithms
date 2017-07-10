40535fa82188d868cdd3608441ce630c59048217
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-09 23:13:54 -0300

    Fix output

--- 0x02-more_search_algorithms/100-jump.c
+++ 0x02-more_search_algorithms/100-jump.c
@@ -50,7 +50,7 @@ int jump_search(int *array, size_t size, int value)
 		print(a, array[a]);
 	}
 
-	printf("Value found between [%lu] and [%lu]\n", a, b);
+	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
 	while (array[a] < value)
 	{
 		print(a, array[a]);
