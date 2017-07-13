d950e57cf7d4cf13faa2ff4e207b03fa167f4cf4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-13 16:27:06 -0300

    Add docstring to binary_tree_height function

--- 0x03-binary_trees/9-binary_tree_height.c
+++ 0x03-binary_trees/9-binary_tree_height.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of a binary tree
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t ltree, rtree;
