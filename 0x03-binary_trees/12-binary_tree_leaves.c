4c560011c9804434407dc1826c32c493469ce2dd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-13 23:57:51 -0300

    Add task 12, first commit

---
+++ 0x03-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
