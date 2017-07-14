218932af58d7e8686291406fcf2e096b955f2994
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-13 22:59:43 -0300

    Add docstring to binary_tree_size function

--- 0x03-binary_trees/11-binary_tree_size.c
+++ 0x03-binary_trees/11-binary_tree_size.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size of
+ *
+ * Return: if tree is null, 0. Otherwise the size of the tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
