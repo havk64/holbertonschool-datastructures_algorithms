14c4601d4c96e6c2daec906ed723e69eacf28942
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-13 21:46:26 -0300

    Add docstring to binary_tree_depth function

--- 0x03-binary_trees/10-binary_tree_depth.c
+++ 0x03-binary_trees/10-binary_tree_depth.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @node: pointer to the node to measure the depth of
+ *
+ * Return: if node is null, 0. Otherwise the depth of the tree
+ */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
 	if (node == NULL)
