99386d0d097bbc61a2dc4d850d052da4fe83a14c
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-13 22:57:55 -0300

    Task 11, first commit

---
+++ 0x03-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
