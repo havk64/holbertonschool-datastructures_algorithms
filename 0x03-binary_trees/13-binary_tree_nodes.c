529b2417db6ba1a105ad6bd8e6664eb9f209ee63
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-15 19:23:09 -0300

    Add docstring to binary_tree_nodes function

--- 0x03-binary_trees/13-binary_tree_nodes.c
+++ 0x03-binary_trees/13-binary_tree_nodes.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * @tree: a pointer to the root node of the tree to count the nodes in
+ *
+ * Return: if node is null 0, otherwise the size of nodes
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
