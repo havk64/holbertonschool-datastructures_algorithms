b4f43321dc20c976d1b02b4d127e835abde8ee1e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-16 03:04:26 -0300

    Fix NULL case

--- 0x03-binary_trees/15-binary_tree_is_full.c
+++ 0x03-binary_trees/15-binary_tree_is_full.c
@@ -8,7 +8,10 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree != NULL && tree->left == NULL && tree->right == NULL)
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if (tree->left != NULL && tree->right != NULL)
