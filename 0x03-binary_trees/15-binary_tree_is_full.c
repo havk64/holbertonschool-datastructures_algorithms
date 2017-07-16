df865ff3ecf75e0dbada72b5939d9e0f78fe504a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-16 02:51:06 -0300

    Add docstring to binary_tree_is_full function

--- 0x03-binary_trees/15-binary_tree_is_full.c
+++ 0x03-binary_trees/15-binary_tree_is_full.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_full - Check if a binary tree is full
+ * @tree: pointer to the root node to the tree to check
+ *
+ * Return: 0 if false, 1 if true
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree != NULL && tree->left == NULL && tree->right == NULL)
