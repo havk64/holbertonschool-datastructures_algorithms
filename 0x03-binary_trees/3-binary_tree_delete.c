f4d7e3cd93f83950d60edd6d5de8e9a4acb80476
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-22 00:37:23 -0300

    Indentation

--- 0x03-binary_trees/3-binary_tree_delete.c
+++ 0x03-binary_trees/3-binary_tree_delete.c
@@ -12,12 +12,10 @@ void binary_tree_delete(binary_tree_t *tree)
 		return;
 
 	if (tree->right != NULL)
-	{
 		binary_tree_delete(tree->right);
-	}
+
 	if (tree->left != NULL)
-	{
 		binary_tree_delete(tree->left);
-	}
+
 	free(tree);
 }
