18987aa9c863614105d59c45751e9c930bde92a5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-22 00:35:11 -0300

    Fix case where tree is NULL

--- 0x03-binary_trees/1-binary_tree_insert_left.c
+++ 0x03-binary_trees/1-binary_tree_insert_left.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
