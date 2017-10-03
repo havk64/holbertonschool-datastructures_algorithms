0c198113bb23e6a7486255edd69d06b94350d54a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-02 21:13:51 -0300

    Add docstring to functions

--- 0x04-huffman_coding/heap/heap_delete.c
+++ 0x04-huffman_coding/heap/heap_delete.c
@@ -1,5 +1,11 @@
 #include "heap.h"
 
+/**
+ * heap_delete - deallocate a entire heap structure
+ * @heap: a pointer to a heap structure
+ * @free_data: a pointer to a function to free/deallocate the structure
+ * Return: Always void.
+ */
 void heap_delete(heap_t *heap, void (*free_data)(void *))
 {
 	binary_tree_node_t *node;
