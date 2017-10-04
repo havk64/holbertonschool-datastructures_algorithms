f5a87c6454d615b6e7260c51f6ef8655574fadae
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-04 20:08:30 -0300

    Add seek_position before get rid of it

--- 0x04-huffman_coding/heap/heap_extract.c
+++ 0x04-huffman_coding/heap/heap_extract.c
@@ -1,5 +1,35 @@
 #include "heap.h"
 
+/**
+ * seek_position - find the place to insert a node at the end of a binary heap
+ * @n: the size of the binary heap
+ * @size: a pointer to a variable to be used to insert the node
+ * Return: On success, an array of _Bool objects. On failure, NULL
+ */
+static _Bool *seek_position(unsigned int n, unsigned short *size)
+{
+	unsigned int i = 0, bufsize = 0, x = 1, j;
+	_Bool buf[32], *bits;
+
+	while (n > (x - 1))
+	{
+		buf[bufsize] = (n & x) != 0;
+		x <<= 1;
+		bufsize++;
+	}
+
+	bits = malloc(sizeof(_Bool) * bufsize);
+	if (bits == NULL)
+		return (NULL);
+
+	*size = bufsize;
+	for (i = 0, j = (bufsize - 1); i < bufsize; i++, j--)
+	{
+		bits[i] = buf[j];
+	}
+	return (bits);
+}
+
 static binary_tree_node_t *swap_firstlast(binary_tree_node_t *last,
 					  binary_tree_node_t *first)
 {
