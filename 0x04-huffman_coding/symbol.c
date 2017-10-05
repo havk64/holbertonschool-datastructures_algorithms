e33f650d80936406bd882ee7025c49c98b95852e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-05 19:35:04 -0300

    Add docstring to functions

--- 0x04-huffman_coding/symbol.c
+++ 0x04-huffman_coding/symbol.c
@@ -1,5 +1,11 @@
 #include "huffman.h"
 
+/**
+ * symbol_create - creates a symbol structure
+ * @data: the data to be stored in the structure
+ * @freq: the data's associated frequency
+ * Return: a pointer to the new structure or NULL on failure
+ */
 symbol_t *symbol_create(char data, size_t freq)
 {
 	symbol_t *symbol;
