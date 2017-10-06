4384515e17465fa1604dcd7546a80aafcfc28457
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-06 18:50:43 -0300

    Task 6 solution, first commit

---
+++ 0x04-huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,36 @@
+#include "huffman.h"
+#include "heap.h"
+#include <stdio.h>
+
+int data_cmp_sym(void *first, void *second)
+{
+	binary_tree_node_t *nested1, *nested2;
+	symbol_t *data1, *data2;
+
+	nested1 = (binary_tree_node_t *)first;
+	nested2 = (binary_tree_node_t *)second;
+	data1 = (symbol_t *)nested1->data;
+	data2 = (symbol_t *)nested2->data;
+	return ((int)(data1->freq - data2->freq));
+}
+
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+	heap_t *heap;
+	symbol_t *symbol;
+	void *nested;
+	size_t i;
+	binary_tree_node_t *node;
+
+	heap = heap_create(data_cmp_sym);
+	for (i = 0; i < size; i++)
+	{
+		symbol = symbol_create(data[i], freq[i]);
+		nested = binary_tree_node(NULL, symbol);
+		node = heap_insert(heap, nested);
+		if (node == NULL)
+			return (NULL);
+	}
+
+	return (heap);
+}
