bb7df79ddd6938ddc914cf30cd61344e4073f2af
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-08 22:47:33 -0300

    Fix issue to extract last node

--- 0x04-huffman_coding/huffman_tree.c
+++ 0x04-huffman_coding/huffman_tree.c
@@ -11,17 +11,19 @@
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
 	heap_t *priority_queue;
-	size_t i, x;
+	binary_tree_node_t *last;
+	size_t x;
 
 	priority_queue = huffman_priority_queue(data, freq, size);
 	if (priority_queue == NULL || data == NULL || freq == NULL)
 		return (NULL);
 
-	for (i = 0; i < (size / 2); i++)
+	while (priority_queue->size > 1)
 	{
 		x = huffman_extract_and_insert(priority_queue);
 		if (x == 1)
-			return (NULL);
+			break;
 	}
-	return (priority_queue->root);
+	last = insert_last(priority_queue);
+	return (last);
 }
