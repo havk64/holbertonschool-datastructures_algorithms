c742b25b71fdf2c255e3280ac2013d2b85bb5cdb
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-09 00:04:58 -0300

    Fix deallocation/error issues

--- 0x04-huffman_coding/heap/heap_delete.c
+++ 0x04-huffman_coding/heap/heap_delete.c
@@ -8,11 +8,19 @@
  */
 static void free_node(binary_tree_node_t *node, void (*free_data)(void *))
 {
+	if (node == NULL)
+		return;
+
 	if (node->left)
 		free_node(node->left, free_data);
 
 	if (node->right)
 		free_node(node->right, free_data);
+
+	if (free_data)
+		free_data(node->data);
+
+	free(node);
 }
 
 /**
@@ -23,10 +31,9 @@ static void free_node(binary_tree_node_t *node, void (*free_data)(void *))
  */
 void heap_delete(heap_t *heap, void (*free_data)(void *))
 {
-	if (heap == NULL || heap->root == NULL || free_data == NULL)
+	if (heap == NULL)
 		return;
 
 	free_node(heap->root, free_data);
-	free_data(heap->root);
 	free(heap);
 }
