8e77c023c8c5d3c096236451d7d956c6436081d6
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-02-28 18:04:43 -0800

    Add docstring to functions

--- 0x06-graphs/0-graph_create.c
+++ 0x06-graphs/0-graph_create.c
@@ -1,5 +1,10 @@
 #include "graphs.h"
 
+/**
+ * graph_create - Allocates memory to a graph_t struct and initializes it.
+ *
+ * Return: On success a graph_t struct, on failure NULL
+ */
 graph_t *graph_create(void)
 {
 	graph_t *graph;
