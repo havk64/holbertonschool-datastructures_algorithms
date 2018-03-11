f42392b2b51e00865d808f4f6ee84f092cad21f8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-03-11 13:59:00 -0700

    Transfer allocation to auxiliary function

--- 0x06-graphs/1-graph_add_vertex.c
+++ 0x06-graphs/1-graph_add_vertex.c
@@ -1,36 +1,6 @@
 #include "graphs.h"
 
-vertex_t *graph_add_vertex(graph_t *graph, const char *str)
-{
-	vertex_t *vertex;
-	char *content;
-
-	if (graph->nb_vertices != 0)
-	{
-		/* Check if str already is in other vertex and exit if so */
-		return (NULL);
-	}
-	/* Case where graph is empty */
-	vertex = malloc(sizeof(vertex_t));
-	if (vertex == NULL)
-		return (NULL);
-
-	content = malloc(strlen(str));
-	if (content == NULL)
-		return (NULL);
-
-	strcpy(content, str);
-	vertex->content = content;
-	vertex->nb_edges = 0;
-	vertex->edges = NULL;
-
-	/* TODO - insert vertix to graph and update nb_vertices */
-	graph->vertices = vertex;
-	graph->nb_vertices = 1;
-	return (vertex);
-}
-
-vertex_t *allocate_vertex(const char *str, size_t index)
+inline vertex_t *allocate_vertex(const char *str, size_t index)
 {
 	vertex_t *vertex = NULL;
 	char *content;
@@ -46,5 +16,36 @@ vertex_t *allocate_vertex(const char *str, size_t index)
 	strcpy(content, str);
 	vertex->content = content;
 	vertex->index = index;
+	vertex->nb_edges = 0;
+	vertex->edges = NULL;
+	return (vertex);
+}
+
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *vertex, *node;
+	char *content;
+	size_t index = 0;
+
+	node = graph->vertices;
+	/* Check if str already is in other vertex and exit if so */
+	for (index = 0;; index++)
+	{
+
+		if (strcmp(str, node->content) == 0)
+			return (NULL);
+
+		node = node->next;
+	}
+	/* Case where graph is empty */
+	vertex = allocate_vertex(str, index);
+
+	/* TODO - insert vertex to graph and update nb_vertices */
+	if (!graph->vertices)
+		graph->vertices = vertex;
+	else
+		node->next = vertex;
+
+	graph->nb_vertices += 1;
 	return (vertex);
 }
