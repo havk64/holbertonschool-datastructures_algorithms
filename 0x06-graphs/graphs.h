65ea1b28fa60b78f33197a5795a6243720c3877d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-03-13 08:35:18 -0700

    Update graphs.h, task 3

--- 0x06-graphs/graphs.h
+++ 0x06-graphs/graphs.h
@@ -70,4 +70,5 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str);
 void graph_display(const graph_t *graph);
 int graph_add_edge(graph_t *graph, const char *src, const char *dest,
 		   edge_type_t type);
+void graph_delete(graph_t *graph);
 #endif /* GRAPHS_H */
