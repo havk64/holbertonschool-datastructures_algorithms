1cb9666d14d9bd29957ef2b2ef2f815fd4e8f273
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-03-30 19:03:29 -0700

    Add docstring to functions

--- 0x05-red_black_tree/0-rb_tree_node.c
+++ 0x05-red_black_tree/0-rb_tree_node.c
@@ -1,5 +1,13 @@
 #include "rb_trees.h"
 
+/**
+ * rb_tree_node - Description
+ * @parent: parent is a pointer to the parent node of the node to create
+ * @value: is the value to put in the new node
+ * @color: is the color of the node
+ * Return: On success, a pointer to the recently created node.
+ * On failure, NULL.
+ */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 	rb_tree_t *node;
