2feb796e380a44d2cb193f7017c267615d4f3923
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-03-30 19:02:54 -0700

    Task 0, completed. First commit

--- 0x05-red_black_tree/0-rb_tree_node.c
+++ 0x05-red_black_tree/0-rb_tree_node.c
@@ -2,4 +2,16 @@
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
+	rb_tree_t *node;
+
+	node = malloc(sizeof(rb_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->color = color;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
