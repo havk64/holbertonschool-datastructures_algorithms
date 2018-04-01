feeb3c1f1d5408a938dd56e135217c6763280340
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-03-31 21:14:52 -0700

    Add task 1 prototype to header file

--- 0x05-red_black_tree/rb_trees.h
+++ 0x05-red_black_tree/rb_trees.h
@@ -34,5 +34,6 @@ typedef struct rb_tree_s
 } rb_tree_t;
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+int rb_tree_is_valid(const rb_tree_t *tree);
 
 #endif /* RB_TREES_H */
